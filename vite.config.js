cb0ffa86327ece87ca03c372dc03977b1d0caeb2
NeuromorphicPhoton <simonriley.ghost.x9@gmail.com>
2025-01-13 18:16:57 +0530

    Deploy Action

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-gh-pages/',//Change this to your repository name Portfolio if portfolio-gh-pages isn't workking
   plugins: [svelte()],
 })
